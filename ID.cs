426553818b82d7cf810edf1e408d18e3095df995
dashakiska69 <dashakiska69@gmail.com>
2018-03-16 15:01:07 +0300

    Создал 2 новых класса

---
+++ ID.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class ID
+    {
+        public int num
+        {
+            get; set;
+        }
+        //Конструктор
+        public ID(int num)
+        {
+            this.num = num;
+        }
+        public void reWriteId()
+        {
+            Console.WriteLine("Номер: {0}\n", num)
+        }
+    }
+}
